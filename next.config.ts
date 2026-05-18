e5ce38e4846d4ff63d8890b1efeb6f001a6fdc6d
snighedis <64488793+snighedis@users.noreply.github.com>
2026-05-18 16:39:22 +0200

    Redirect EmailMagnet Chrome extension URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/emailmagnet/chrome-extension",
+        destination: "/emailmagnet/chrome-email-extractor",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
